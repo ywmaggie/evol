9a078e95b970fdb37f28d61d170396c4c2b3103f
Yuwei Zhang <yuweiz@fb.com>
2017-12-22 01:18:57 -0800

    routing

--- app.js
+++ app.js
@@ -1,81 +1,12 @@
-// var express = require('express');
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-//
-// var index = require('./src/routes/index');
-// var users = require('./src/routes/users');
-//
-// var app = express();
-//
-// // view engine setup
-// // app.set('views', path.join(__dirname, 'src/components'));
-// // app.set('view engine', 'jsx');
-// // app.engine('jsx', require('express-react-views').createEngine());
-//
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'src/static')));
-//
-// // app.use('/', index);
-// app.use('/users', users);
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-//
-// module.exports = app;
-
-
 const express = require('express');
 const path = require('path');
-// const generatePassword = require('password-generator');
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
-
 app.get('/users', function(req, res, next) {
-	// Comment out this line:
-  //res.send('respond with a resource');
-
-  // And insert something like this instead:
   res.json([{
   	id: 1,
   	username: "samsepi0l"
@@ -93,5 +24,3 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,99 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, {Component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// class App extends Component {
+//   state = {
+//     users: [],
+//     value: 1
+//   };
+//
+//   componentDidMount() {
+//     fetch('/users').then(res => res.json()).then(users => this.setState({users}));
+//   }
+//
+//   render() {
+//     return (<div className="App">
+//       <div onClick={() => this.setState({value: 2})}>
+//         {this.state.value}
+//       </div>
+//       <h1>Users</h1>
+//       <img src={'1011.png'}/>
+//       {this.state.users.map(user => <div key={user.id}>{user.username}</div>)}
+//     </div>);
+//   }
+// }
+//
+// export default App;
 
-class App extends Component {
-  state = {
-    users: [],
-    value: 1
-  };
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Pic from './pic';
 
-  componentDidMount() {
-    fetch('/users').then(res => res.json()).then(users => this.setState({users}));
-  }
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
 
-  render() {
-    return (<div className="App">
-      <div onClick={() => this.setState({value: 2})}>
-        {this.state.value}
-      </div>
-      <h1>Users</h1>
-      {this.state.users.map(user => <div key={user.id}>{user.username}</div>)}
-    </div>);
-  }
-}
+      <hr/>
 
-export default App;
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <Pic />
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+export default BasicExample
